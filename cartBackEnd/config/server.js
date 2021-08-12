403b62d82dc5c22bb0069bb9d530904fc371569b
ErikRice <erik.m.rice12@gmail.com>
2021-08-11 23:18:56 -0400

    first commit

---
+++ cartBackEnd/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 8082),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9065ca332388d269432f238d90a53893'),
+    },
+  },
+});
---
+++ cartBackEnd/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fed3af7d-24c9-4b8e-becf-8567ad2da4c6'
+};
